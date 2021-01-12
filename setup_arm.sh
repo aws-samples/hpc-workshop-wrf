cf5f7b4754425420aea911a74c64819a7ac62d2b
Federico D'Alessio <dalesf@amazon.it>
2021-01-12 18:09:19 +0000

    new file:   setup_arm.sh 	new file:   wrf_setup_scripts/compile_and_install_arm.sh 	new file:   wrf_setup_scripts/wps_configure.default.patch 	new file:   wrf_setup_scripts/wrf_configure_new.default.patch

---
+++ setup_arm.sh
@@ -0,0 +1,121 @@
+#!/bin/bash -i
+. /etc/parallelcluster/cfnconfig
+
+shared_folder=$(echo $cfn_shared_dir | cut -d ',' -f 1 )
+
+
+function create_env_file {
+echo "Create Env"
+cat <<@EOF >${shared_folder}/gcc_setup_env.sh
+#!/bin/bash
+export SHARED_DIR=${shared_folder}
+export SETUP_DIR=${shared_folder}/hpc-workshop-wrf
+export BUILDDIR=${shared_folder}/build/gccWRF
+export DIR=${shared_folder}/gccWRF
+export SCRIPTDIR=${shared_folder}/gccWRF/bin
+export CC=gcc
+export CXX=g++
+export FC=gfortran
+export FCFLAGS=-m64
+export F77=gfortran
+export FFLAGS=-m64
+export JASPERLIB=${shared_folder}/gccWRF/grib2/lib
+export JASPERINC=${shared_folder}/gccWRF/grib2/include
+export LDFLAGS=-L${shared_folder}/gccWRF/grib2/lib
+export CPPFLAGS=-I${shared_folder}/gccWRF/grib2/include
+export PATH=${shared_folder}/gccWRF/netcdf/bin:${shared_folder}/gccWRF/bin:\$PATH
+export NETCDF=${shared_folder}/gccWRF/netcdf
+export JASPERLIB=${shared_folder}/gccWRF/grib2/lib
+export JASPERINC=${shared_folder}/gccWRF/grib2/include
+export LD_LIBRARY_PATH=${shared_folder}/gccWRF/netcfd/lib
+export TARGET_DIR=\${SHARED_DIR}/FORECAST/domains/test/
+export GEOG_BASE_DIR=\${SHARED_DIR}/FORECAST/domains/
+export WRF_DIR=\${DIR}/WRFV3-3.9.1.1
+export WPS_DIR=\${DIR}/WPS
+
+
+
+export OMP_NUM_THREADS=2
+
+@EOF
+
+chmod 777 ${shared_folder}
+chmod 755 ${shared_folder}/gcc_setup_env.sh
+rm -f ${shared_folder}/setup_env.sh
+ln -s ${shared_folder}/gcc_setup_env.sh ${shared_folder}/setup_env.sh
+}
+
+echo "NODE TYPE: ${cfn_node_type}"
+
+case ${cfn_node_type} in
+        MasterServer)
+                echo "I am Master node"
+                create_env_file
+                source ${shared_folder}/setup_env.sh
+                cd wrf_setup_scripts
+                /bin/su ec2-user -c "/bin/bash -i compile_and_install.sh"
+                /bin/su ec2-user -c "/bin/bash -i build_dir.sh"
+
+        ;;
+        ComputeFleet)
+                echo "I am a Compute node"
+        ;;
+        esac
+
+# Set ulimits according to WRF needs
+cat >>/tmp/limits.conf << EOF
+# core file size (blocks, -c) 0
+*           hard    core           0
+*           soft    core           0
+
+# data seg size (kbytes, -d) unlimited
+*           hard    data           unlimited
+*           soft    data           unlimited
+
+# scheduling priority (-e) 0
+*           hard    priority       0
+*           soft    priority       0
+
+# file size (blocks, -f) unlimited
+*           hard    fsize          unlimited
+*           soft    fsize          unlimited
+
+# pending signals (-i) 256273
+*           hard    sigpending     1015390
+*           soft    sigpending     1015390
+
+# max locked memory (kbytes, -l) unlimited
+*           hard    memlock        unlimited
+*           soft    memlock        unlimited
+
+# open files (-n) 1024
+*           hard    nofile         65536
+*           soft    nofile         65536
+
+# POSIX message queues (bytes, -q) 819200
+*           hard    msgqueue       819200
+*           soft    msgqueue       819200
+
+# real-time priority (-r) 0
+*           hard    rtprio         0
+*           soft    rtprio         0
+
+# stack size (kbytes, -s) unlimited
+*           hard    stack          unlimited
+*           soft    stack          unlimited
+
+# cpu time (seconds, -t) unlimited
+*           hard    cpu            unlimited
+*           soft    cpu            unlimited
+
+# max user processes (-u) 1024
+*           soft    nproc          16384
+*           hard    nproc          16384
+
+# file locks (-x) unlimited
+*           hard    locks          unlimited
+*           soft    locks          unlimited
+EOF
+
+sudo bash -c 'cat /tmp/limits.conf > /etc/security/limits.conf'
+exit 0
---
+++ wrf_setup_scripts/compile_and_install_arm.sh
@@ -0,0 +1,175 @@
+#!/bin/bash
+env > /tmp/env_before
+source /shared/setup_env.sh
+
+env > /tmp/env_after
+
+#Download source packages
+#bash download.sh
+#Run Manually because it takes too much to be included in cluster preparation
+#bash download_and_install_geog_data.sh &
+
+mkdir -p $BUILDDIR
+################## COMPILE HD5  ##################################################
+cd $BUILDDIR
+tar -zxvf /shared/download/hdf5-1.10.5.tar.gz
+cd hdf5-1.10.5
+./configure CC=mpicc FC=mpif90 CXX=mpicxx --enable-parallel --enable-fortran --prefix=$DIR/netcdf --with-pic
+sed -i -e 's#wl=""#wl="-Wl,"#g' libtool
+sed -i -e 's#pic_flag=""#pic_flag=" -fPIC -DPIC"#g' libtool
+make -j 4
+make install
+
+
+################## COMPILE NETCDF ################################################
+cd $BUILDDIR
+tar -zxvf /shared/download/netcdf-c-4.7.4.tar.gz
+cd netcdf-c-4.7.4
+
+bash <<@EOF
+export CC=mpicc
+export CXX=mpicxx
+export F77=mpif77
+export FC=mpif90
+export MPICC=mpicc
+export MPIFC=mpif90
+export MPICXX=mpicxx
+
+./configure --prefix=$DIR/netcdf CPPFLAGS="-I$DIR/netcdf/include" CFLAGS="-DHAVE_STRDUP -O3 -mcpu=native" LDFLAGS="-L$DIR/netcdf/lib" --enable-shared --enable-netcdf-4  --with-pic --disable-doxygen --disable-dap
+make -j 4
+make install
+@EOF
+
+#
+cd $BUILDDIR
+tar -zxvf /shared/download/netcdf-fortran-4.5.3.tar.gz
+bash <<@EOF
+cd netcdf-fortran-4.5.3
+export CC=gcc
+export CXX=g++
+export F77=gfortran
+export FC=gfortran
+export MPICC=mpicc
+export MPIFC=mpif90
+export MPICXX=mpicxx
+
+export NFDIR=$DIR/netcdf
+export CFLAGS="-L$DIR/netcdf/lib -I$DIR/netcdf/include" 
+export CXXFLAGS="-L$DIR/netcdf/lib -I$DIR/netcdf/include"
+export FCFLAGS="-L$DIR/netcdf/lib -I$DIR/netcdf/include"
+export FFLAGS="-L$DIR/netcdf/lib -I$DIR/netcdf/include"
+export LDFLAGS="-L$DIR/netcdf/lib"
+export LIBS=" -lnetcdf -lhdf5_hl -lhdf5 -lz -lcurl"
+# Need netcdf, hdf5 and libcurl libs in library path to allow configure tests to succeed
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$DIR/netcdf/lib
+./configure  --prefix=$DIR/netcdf --enable-shared --with-pic --disable-doxygen
+make -j4
+make check
+make install
+@EOF
+
+################## COMPILE NETCDF ################################################
+cd $BUILDDIR
+tar -zxvf /shared/download/zlib-1.2.11.tar.gz
+cd zlib-1.2.11
+./configure --prefix=$DIR/grib2
+make -j 4
+make install
+
+
+################## COMPILE LIBPNG ################################################
+cd $BUILDDIR
+tar -zxvf /shared/download/libpng-1.2.57.tar.gz
+cd libpng-1.2.57
+./configure --prefix=$DIR/grib2
+make -j 4
+make install
+
+
+################## COMPILE JASPER ################################################
+cd $BUILDDIR
+tar -zxvf /shared/download/jasper-1.900.2.tar.gz
+cd jasper-1.900.2
+./configure --prefix=$DIR/grib2
+make -j 4
+make install
+
+################## COMPILE WRF    ################################################
+cd $DIR
+tar -zxvf /shared/download/WRFV3.9.1.1.TAR.gz
+cd WRFV3
+
+echp "arch/configure_new.defaults < ${SETUP_DIR}/wrf_configure_new.defaults"
+patch arch/configure_new.defaults < ${SETUP_DIR}/wrf_setup_scripts/wrf_configure_new.defaults.patch
+
+#Copy file config.wrf instead of manual configuration
+#./configure step with option:
+#                   4 dm+sm GCC (gfortran/gcc): Aarch64
+#                   1 (basic)
+#cp /shared/download/configure.wrf_demo ./configure.wrf
+./configure <<@EOF
+4
+1
+@EOF
+
+#Compile in parallel to speed up compilation process
+./compile -j 4 em_real >compile.log 2>&1
+#Compile in sequence in order to solve unmanaged parallel compilation issues
+./compile  em_real >compile.log 2>&1
+
+cd $DIR
+ln -s WRFV3 WRFV3-3.9.1.1
+
+################## COMPILE WPS    ################################################
+cd $DIR
+tar -zxvf /shared/download/WPSV3.9.1.TAR.gz
+cd WPS
+
+echp "arch/configure_new.defaults < ${SETUP_DIR}/wrf_configure_new.defaults"
+patch arch/configure_new.defaults < ${SETUP_DIR}/wrf_setup_scripts/wrf_configure_new.defaults.patch
+
+#REPLACED WITH STATIC CONFIG FILE
+#./configure step with option:
+#                   3 (dmpar)
+
+# FIX added option -lgomp to WRF_LIB variable in configure.wps
+#cp /shared/download/configure.wps_demo ./configure.wps
+./configure <<@EOF
+1
+@EOF
+
+echp "configure.wps < ${SETUP_DIR}/wrf_setup_scripts"
+patch configure.wps < ${SETUP_DIR}/wrf_setup_scripts/configure.wps.patch
+
+#Compile WPS
+./compile  >compile.log 2>&1
+
+
+################## COMPILE ncview  ################################################
+cd $BUILDDIR
+tar -zxvf /shared/download/ncview-1.93g.tar.gz
+cd ncview-1.93g
+sudo yum -y install libXaw-devel
+./configure --prefix=/shared/gccWRF --with-netcdf_incdir=$NETCDF/include --with-netcdf_libdir=$NETCDF/lib
+make
+make install
+ 
+################## COMPILE wgrib2  ################################################
+cd $BUILDDIR
+tar -zxvf /shared/download/wgrib2.tgz
+cd grib2
+bash <<@EOF
+export FC=gfortran
+export COMP_SYS=gnu_linux
+
+cp /usr/share/automake-*/config.guess ./jasper-1.900.1/acaux/config.guess
+cp /usr/share/automake-*/config.guess ./jasper-1.900.1/config.guess
+cp /usr/share/automake-*/config.guess ./netcdf-3.6.3/config.guess
+
+make
+cp wgrib2/wgrib2  $DIR/bin
+
+@EOF
+
+#Wait for Async geog data to complete
+wait
