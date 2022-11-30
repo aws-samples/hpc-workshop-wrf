8008956cbee897cbe3d08e808c0f5324d6052f4d
Federico D'Alessio <dalesf@amazon.it>
2022-11-30 17:44:09 +0000

    modified:   wrf_setup_scripts/compile_and_install_spack.sh 	deleted:    wrf_setup_scripts/download.sh

--- wrf_setup_scripts/compile_and_install_spack.sh
+++ wrf_setup_scripts/compile_and_install_spack.sh
@@ -5,28 +5,12 @@ source /shared/setup_env.sh
 
 env > /tmp/env_after
 
-#Create Spack environment
-spack env create wrf-workshop-env
-spack env activate wrf-workshop-env
-
-#Install and configure compiler and MPI libraries
-spack install intel-oneapi-compilers@2021.4.0
-spack compiler add `spack location -i intel-oneapi-compilers`/compiler/latest/linux/bin/intel64
-spack compiler add `spack location -i intel-oneapi-compilers`/compiler/latest/linux/bin
-
-
-spack config add "packages:all:compiler:[intel, oneapi, gcc]"
-spack config add "packages:all:providers:mpi:[intel-oneapi-mpi]"
-
-spack install intel-oneapi-mpi@2021.4.0%intel
-
-spack load intel-oneapi-mpi
 
 #Build packages
-spack install    wrf@4.2%intel
+spack install    wrf@4.2
 
-spack install    wps@4.2%intel
+spack install    wps@4.2
 
-spack install    wgrib2%intel
+spack install    wgrib2
 
-spack install    ncview@4.2%intel
+spack install    ncview@4.2
