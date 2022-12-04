02c99aee947df56ef0dfba12ba032343bfa014dd
Federico D'Alessio <dalesf@amazon.it>
2022-12-04 19:00:02 +0000

    modified:   wrf_setup_scripts/compile_and_install_spack.sh

--- wrf_setup_scripts/compile_and_install_spack.sh
+++ wrf_setup_scripts/compile_and_install_spack.sh
@@ -5,13 +5,13 @@ source /shared/setup_env.sh
 
 
 #Build packages
-spack add    wrf@4.4
+spack install wps   #This is going to install WPS and WRF
 
-spack add    wps@4.3
+spack install ncview
 
-spack add    ncview@4.4
+#spack install    wgrib2
+#Manual install wgrib2 because of spack open issue on Graviton2
 
-spack install    wgrib2
 
 
 cat <<@EOF >> /shared/setup_env.sh
@@ -20,8 +20,15 @@ spack load wps
 spack load ncview
 
 
-export WRF_DIR=\$(spack find --path wrf | grep '^wrf' | awk '{print (\$2)'})
-export WPS_DIR=\$(spack find --path wps | grep '^wps' | awk '{print (\$2)'})
+export WRF_DIR=\$(spack location -i wrf)
+export WPS_DIR=\$(spack location -i wps)
+export NETCDF=\$(spack location -i netcdf-fortran)
+export JASPERDIR=\$(spack location -i jasper@1.900.1)
 
+export JASPERLIB=\$JASPERDIR/lib
+export JASPERINC=\$JASPERDIR/include
+
+
+export 
 @EOF
 
